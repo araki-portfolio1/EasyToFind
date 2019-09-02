8a6e52fd36189db0a09d4f6c48bdf8472514bb04
araki-takahiro <sanju13gh60ki@gmail.com>
2019-09-02 10:39:20 +0000

    after_sign_up_path_for image_select

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,16 @@
 class Public::UsersController < Public::ApplicationController
     def show
     end
+
+    def edit
+    end
+
+    def image_select
+        @user = User.find(params[:id])
+    end
+
+    private
+    def user_params
+        params.require(:users).permit(:name, :email, :image)
+    end
 end
